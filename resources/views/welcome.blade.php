37681f0247016052902710d1dc9eeea608d37114
CedricStrub <strubcedric@gmail.com>
2023-06-13 16:52:58 +0200

    CSS home + footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,44 @@
 @include('header')
 
-
+<div class="container">
+    <div class="c-spacer"></div>
+    <div class="card">
+        <div class="c-head">
+            <div class="c-t-left">
+                <p class="c-title">Titre du projet</p>  
+            </div>
+            <div class="c-t-right">
+                <img src="{{ asset('../images/ajouter-la-liste.png') }}" class="c-t-img">
+                <img src="{{ asset('../images/solution.png') }}" class="c-t-img">
+            </div>
+        </div>
+        <div class="c-body">
+            <img src="" alt="" class="c-miniature">
+            <div class="c-description">
+                <p>Description du projet brève avec une photo représentative au dessus de manière a situer correctement son projet</p>
+            </div>
+            <div class="c-footer">
+                <div class="c-stat">304 vue</div>
+                <div class="c-author">machin</div>
+            </div>
+        </div>
+    </div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+</div>
 
 
 

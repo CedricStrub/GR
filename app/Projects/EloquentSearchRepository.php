53edf7f7b63753ea11bf7cbb1d386e9d3e2bce05
CedricStrub <strubcedric@gmail.com>
2023-06-20 10:31:57 +0200

    up

--- app/Projects/EloquentSearchRepository.php
+++ app/Projects/EloquentSearchRepository.php
@@ -4,13 +4,13 @@
  use Illuminate\Database\Eloquent\Collection;
  class EloquentSearchRepository implements SearchRepository
  {
-    // public function search(string $term): Collection
-    // {
-    //     return Project::query()
-    //         ->where(fn ($query) => (
-    //             $query->where('nom', 'LIKE', "%{$term}%")
-    //                 ->orWhere('description', 'LIKE', "%{$term}%")
-    //         ))
-    //         ->get();
-    // }
+    public function search(string $term): Collection
+    {
+        return Project::query()
+            ->where(fn ($query) => (
+                $query->where('nom', 'LIKE', "%{$term}%")
+                    ->orWhere('description', 'LIKE', "%{$term}%")
+            ))
+            ->get();
+    }
  }
